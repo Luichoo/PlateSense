684b0bf706fffaed04f975f63a1764ccefe7ef6c
Luis Blanco <slam46507@live.com.mx>
2023-05-08 02:41:08 -0600

    design navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,18 +4,32 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import logo from './logo.png';
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand><Link to="/" style={{'text-decoration': 'none'}}>PlateSense</Link></Navbar.Brand>
+
+    <Navbar bg="dark" expand="lg" variant="dark" className="pb-3">
+          <Container className="ms-2">
+        <Navbar.Brand>    
+        <img
+              
+              src={logo}
+              width="40 px"
+              height="40 px"
+              className="d-inline-block align-top me-3 mt-0"
+              alt=""
+              style={{'border-radius': '50%'}}
+            />{' '}
+            <Link to="/" style={{'text-decoration': 'none','color':'#FFFFFFC8'}}> PlateSense</Link>
+            </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
+
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -29,8 +43,9 @@ function NavBar() {
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+        </Container>
     </Navbar>
+    
   );
 }
 
