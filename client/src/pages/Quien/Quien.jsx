c477b7a2d14ef43623c34b9d0c253fae730e4f8c
Luis Blanco <slam46507@live.com.mx>
2023-05-08 16:42:11 -0600

    quienes somos view

---
+++ client/src/pages/Quien/Quien.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Quien() {
+    return (
+        <div className="container d-flex justify-content-center flex-column">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="text-center">Quienes somos</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <p className="text-center">Somos un grupo de estudiantes que mediante inteligencia artifical, 
+                    buscamos desarrollar un lector de placas de coche para facilitar las cosas dentro de la universidad en cuestion del estacionamiento
+                        </p>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Quien;
