661d40732c009242ac1a32bd6a504b4c298f4384
Luis Blanco <slam46507@live.com.mx>
2023-05-10 01:11:31 -0600

    navbar changes

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -8,36 +8,70 @@ import logo from './logo.png';
 import './Navbar.css';
 function NavBar() {
   return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand eventKey={1}> <img
+      
+                src={logo}
+                width="40 px"
+                height="40 px"
+                className=" me-3 mt-0"
+                alt=""
+                style={{'border-radius': '50%'}}
+              />{' '}
+              <Link to="/" style={{'text-decoration': 'none','color':'#FFFFFFC8'}}> PlateSense</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
 
-    <Navbar bg="dark" expand="lg" variant="dark" className="pb-3">
-          <Container className="ms-2">
-        <Navbar.Brand>    
-        <img
+        <Nav className="me-auto d-flex flex-row justify-content-between align-content-around">
+          <div className="d-flex flex-row justify-content-end w-100 align-content-end">
+
+   
+          <div className="d-flex flex-row">
+          <Nav.Link eventKey={1}><Link to='/'>Inicio</Link></Nav.Link>
+        <Nav.Link eventKey={2} ><Link to='/informacion' >Quienes somos</Link></Nav.Link>
+
+          </div>
+
+        <div className="d-flex flex-row">
+          <Nav.Link eventKey={3} > <Link to='/register' >Registrate</Link></Nav.Link>
+          <Nav.Link eventKey={4}><Link  to='/login' >Iniciar Sesión</Link></Nav.Link>
+        </div>
+        </div>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+
+    // <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark" className="pb-3">
+    //       <Container className="ms-2">
+    //     <Navbar.Brand>    
+    //     <img
               
-              src={logo}
-              width="40 px"
-              height="40 px"
-              className="d-inline-block align-top me-3 mt-0"
-              alt=""
-              style={{'border-radius': '50%'}}
-            />{' '}
-            <Link to="/" style={{'text-decoration': 'none','color':'#FFFFFFC8'}}> PlateSense</Link>
-            </Navbar.Brand>
+    //           src={logo}
+    //           width="40 px"
+    //           height="40 px"
+    //           className=" me-3 mt-0"
+    //           alt=""
+    //           style={{'border-radius': '50%'}}
+    //         />{' '}
+    //         <Link to="/" style={{'text-decoration': 'none','color':'#FFFFFFC8'}}> PlateSense</Link>
+    //         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto nav_link">
-            <Nav.Link><Link to='/'>Inicio</Link></Nav.Link>
-            <Nav.Link><Link to='/informacion' >Quienes somos</Link></Nav.Link>
-          </Nav>
-          <Nav className="ms-auto nav_link">
-             <Nav.Link><Link to='/register' >Registrate</Link></Nav.Link>
-            <Nav.Link><Link to='/login' >Iniciar Sesión</Link></Nav.Link>
-          </Nav>
+    //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    //     <Navbar.Collapse id="responsive-navbar-nav">
+    //       <Nav className="me-auto nav_link">
+    //         <Nav.Link><Link to='/'>Inicio</Link></Nav.Link>
+    //         <Nav.Link><Link to='/informacion' >Quienes somos</Link></Nav.Link>
+    //       </Nav>
+    //       <Nav className="ms-auto nav_link">
+    //          <Nav.Link><Link to='/register' >Registrate</Link></Nav.Link>
+    //         <Nav.Link><Link to='/login' >Iniciar Sesión</Link></Nav.Link>
+    //       </Nav>
  
-        </Navbar.Collapse>
-        </Container>
-    </Navbar>
+    //     </Navbar.Collapse>
+    //     </Container>
+    // </Navbar>
     
   );
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,88 +5,99 @@ import "./scripts.js";
 import "./Home.css";
 
 function Home() {
-  const [isActive, setIsActive] = useState(false);
-  const [img, setImg] = useState(null);
-  const webcamRef = useRef(null);
-  const [disabled, setDisabled] = useState(false);
-  const [disabled2, setDisabled2] = useState(false);
-  const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: "user",
-  };
-  function handlerWCB() {
-    setDisabled(!disabled);
-  }
-  function handlertoggler() {
-    setDisabled2(!disabled2);
-  }
+	const [isActive, setIsActive] = useState(false);
+	const [img, setImg] = useState(null);
+	const webcamRef = useRef(null);
+	const [disabled, setDisabled] = useState(false);
+	const [disabled2, setDisabled2] = useState(false);
+	const videoConstraints = {
+		width: 1280,
+		height: 720,
+		facingMode: "user",
+	};
+	function handlerWCB() {
+		setDisabled(!disabled);
+	}
+	function handlertoggler() {
+		setDisabled2(!disabled2);
+	}
 
+	const capture = useCallback(() => {
+		const imageSrc = webcamRef.current.getScreenshot();
+		setImg(imageSrc);
+	}, [webcamRef]);
 
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImg(imageSrc);
-  }, [webcamRef]);
+	return (
+		<Container fluid>
+			<div className="d-flex flex-column justify-content-center align-items-center h-100 mt-5">
+				<h2>Bienvenido ese</h2>
+				<div class="switch-button">
+					<input
+						onClick={() => {
+							setIsActive(!isActive);
+							handlerWCB();
+						}}
+						type="checkbox"
+						name="switch-button"
+						id="switch-label"
+						class="switch-button__checkbox"
+						disabled={disabled2}></input>
 
-  return (
-    <Container fluid>
-      <div className="d-flex flex-column justify-content-center align-items-center h-100 mt-5">
-        <h2>Bienvenido ese</h2>
-        <div class="switch-button">
+					<label for="switch-label" class="switch-button__label"></label>
+          <div className="h-75"></div>
+				</div>
+				{img === null ? (
+					<>
+						<div className="cam-container mt-4 h-100">
+							{isActive && (
+								<Webcam
+									audio={false}
+									ref={webcamRef}
+									screenshotFormat="image/jpeg"
+									videoConstraints={videoConstraints}
+									height={338}
+									width={600}
+									className=" wc-image"
+								/>
+							)}
+						</div>
 
-    <input onClick={() => {setIsActive(!isActive); handlerWCB()}} type="checkbox" name="switch-button" id="switch-label" class="switch-button__checkbox" disabled={disabled2}></input>
+						<div className="btn-container mt-4 mb-5">
+							<button
+								onClick={() => {
+									capture();
+									handlertoggler();
+								}}
+								className="btn btn-dark btn-lg"
+								id="submit"
+								disabled={!disabled}>
+								Captura
+							</button>
+						</div>
+					</>
+				) : (
+					<>
+						<div className="img-container mt-4 h-100">
+							<img src={img} alt="screenshot" className="wc-image" />
+						</div>
 
-    <label for="switch-label" class="switch-button__label"></label>
-</div>
-        {img === null ? (
-          <>
-            <div className="cam-container mt-4 h-100">
-
-  
-            {isActive &&  <Webcam
-                audio={false}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                videoConstraints={videoConstraints}
-                height={338}
-                width={600}
-                className=" wc-image"
-              />}
-            </div>
-
-
-            <div className="btn-container mt-4 mb-5">
-              <button
-                onClick={()=>{capture(); handlertoggler()}}
-                className="btn btn-dark btn-lg"
-                id="submit"
-                disabled={!disabled}
-              >
-                Captura
-              </button>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="img-container mt-4 h-100">
-              <img src={img} alt="screenshot" className="wc-image" />
-            </div>
-
-            <div className="btn-container mt-4 mb-5" style={{'marginTop':'1px'}}>
-              <button
-                onClick={() => {setImg(null)}}
-                className="btn btn-dark btn-lg"
-                id="submit"
-              >
-                Repetir
-              </button>
-            </div>
-          </>
-        )}
-
-      </div>
-    </Container>
-  );
+						<div
+							className="btn-container mt-4 mb-5"
+							style={{ marginTop: "1px" }}>
+							<button
+								onClick={() => {
+									setImg(null);
+								}}
+								className="btn btn-dark btn-lg"
+								id="submit">
+								Repetir
+							</button>
+						</div>
+					</>
+				)}
+			</div>
+		</Container>
+	);
 }
 
 export default Home;
