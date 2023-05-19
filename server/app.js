fe02ef36dd95308686d581b675d69c6806af6af1
Luis Blanco <slam46507@live.com.mx>
2023-05-19 07:58:39 -0600

    uso de cors

--- server/app.js
+++ server/app.js
@@ -2,20 +2,31 @@ require('dotenv').config({path: `${__dirname}/.env`});
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const placesRoutes = require('./Routes/places.routes');
+const cors = require('cors');
+
+
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // * = any domain
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // * = any headers
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // * = any methods
+// })
 app.use(express.json());
+app.use('/api/auth',placesRoutes);
 
-
-const mongostring = process.env.MONGOurl;
-if (mongoose.connect(mongostring)) {
-    console.log('Connected to MongoDB');
-} else {
-    console.log('Error connecting to MongoDB');
-}
+// const mongostring = process.env.MONGOurl;
+// if (mongoose.connect(mongostring)) {
+//     console.log('Connected to MongoDB');
+// } else {
+//     console.log('Error connecting to MongoDB');
+// }
 
 app.listen(port, () => {
     console.log('Server is up and running on http://localhost:' + port);
