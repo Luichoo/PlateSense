bdd1070147d01df96bd395dacb935de12d9ec17e
Luis Blanco <slam46507@live.com.mx>
2023-05-21 04:13:32 -0600

    optimizacion db

--- server/app.js
+++ server/app.js
@@ -1,43 +1,39 @@
-require('dotenv').config({path: `${__dirname}/.env`});
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const placesRoutes = require('./Routes/places.routes');
-const cors = require('cors');
-
-
-
+require("dotenv").config({ path: `${__dirname}/.env` });
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const placesRoutes = require("./Routes/places.routes");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*'); // * = any domain
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // * = any headers
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // * = any methods
-// })
+
 app.use(express.json());
-app.use('/api/auth',placesRoutes);
+app.use("/api/auth", placesRoutes);
 
-// const mongostring = process.env.MONGOurl;
-// if (mongoose.connect(mongostring)) {
-//     console.log('Connected to MongoDB');
-// } else {
-//     console.log('Error connecting to MongoDB');
-// }
-
-app.listen(port, () => {
-    console.log('Server is up and running on http://localhost:' + port);
-});
+const mongostring = process.env.MONGOurl;
+mongoose.set('strictQuery', true);
+mongoose
+	.connect(mongostring)
+	.then(() => {
+		console.log("Connected to mongoDB!");
+		app.listen(port, () => {
+			console.log("Server is up and running on http://localhost:" + port);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({ message: error.message || 'An unknown error occurred!' });
-})
+	if (res.headerSent) {
+		return next(error);
+	}
+	res.status(error.code || 500);
+	res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 module.exports = app;
