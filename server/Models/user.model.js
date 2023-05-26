f0bdb657076d60609f114822cdc179f62f642ad5
Luis Blanco <slam46507@live.com.mx>
2023-05-26 01:33:37 -0600

    fix router

--- server/Models/user.model.js
+++ server/Models/user.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User', {
     clave: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 5 },
+    placas :[{type: String}],
     salt: { type: String, required: true },
 });
 
--- server/Routes/places.routes.js
+++ server/Routes/places.routes.js
@@ -6,9 +6,6 @@ const usersController = require('../Controllers/user.controller');
 router.post('/register', usersController.postUser);
 router.post('/login', usersController.postUserLogin);
 router.get('/access', usersController.validateJwt ,usersController.getUserAccess);
-router.get('/', (req, res, next) => {
-    res.send('Hello World!');
-}
-);
+
 
 module.exports = router;
