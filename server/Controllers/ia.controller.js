884bc56b2e25445a4d7a02bbc242bff296580825
Luis Blanco <slam46507@live.com.mx>
2023-06-02 19:42:29 -0600

    semi final

--- server/Controllers/ia.controller.js
+++ server/Controllers/ia.controller.js
@@ -3,35 +3,43 @@ const User = require("../Models/user.model");
 const uploadImage = async (req, res) => {
 	console.log("uploadImage");
 	const body = req.body;
-	const url = req.body.url;
+	const urlimg = req.body.url;
 	try {
 		const user = await User.findOne({ clave: body.clave });
 		if (!user) {
 			return res.status(403).json({ message: "User does not exist" });
 		}
-		/*
+		//aqui cambiar la url por la que se obtenga de la camara
+		await fetch("url aqui", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				url: urlimg,
+			}),
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				console.log("--------");
+				console.log(json.placa);
+				//ponemos la respuesta en mayusculas
+				const placa = json.placa.toUpperCase();
+				const findPlate = user.placas.find((plate) => plate === placa);
 
+				if (!findPlate) {
+					return res.status(200).json({
+						message: "$60 pesos",
+						placa: placa,
+						detectado: false,
+					});
+				}
 
-
-
-        */
-		const placa = "P85BHW";
-		const findPlate = user.placas.find((plate) => plate === placa);
-		if (!findPlate) {
-			return res
-				.status(200)
-				.json({
-					message: "$60 pesos",
+				return res.status(200).json({
+					message: "$25 pesos",
 					placa: placa,
-                    detectado : false
+					detectado: true,
 				});
-		}
-		return res
-			.status(200)
-			.json({
-				message:  "$25 pesos",
-				placa: placa,
-                detectado : true
 			});
 	} catch (err) {
 		console.log(err);
